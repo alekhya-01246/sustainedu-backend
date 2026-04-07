e11eb3c8240b2921b6426187e41fcc2ed5a18172
jeevan2910 <jeevanreddy2910@gmail.com>
2026-04-07 19:23:28 +0530

    fixed search repo

--- src/main/java/com/sustainedu/backend/repository/LessonRepository.java
+++ src/main/java/com/sustainedu/backend/repository/LessonRepository.java
@@ -3,7 +3,11 @@ package com.sustainedu.backend.repository;
 import com.sustainedu.backend.entity.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
+
+    List<Lesson> findByTitleContainingIgnoreCaseOrTopicContainingIgnoreCase(String title, String topic);
+
 }
